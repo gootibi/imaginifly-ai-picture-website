28fbaccb4f905145b90dc3a6636b1752071b7436
gootibi <gootibi@gmail.com>
2024-03-08 01:42:58 +0100

    Imaginify AI photo page project start. Create a project setup, Next.js routing and folder structure, Clerk authentication, layout sidebar and mobildevice navigation, MongoDB and Mongoos - Database and Models setup. Changes to be committed: 	new file:   .env 	new file:   app/(auth)/layout.tsx 	new file:   app/(auth)/sign-in/[[...sign-in]]/page.tsx 	new file:   app/(auth)/sign-up/[[...sign-up]]/page.tsx 	new file:   app/(root)/credits/page.tsx 	new file:   app/(root)/layout.tsx 	new file:   app/(root)/page.tsx 	new file:   app/(root)/profile/page.tsx 	new file:   app/(root)/transformations/[id]/page.tsx 	new file:   app/(root)/transformations/[id]/update/page.tsx 	new file:   app/(root)/transformations/add/[type]/page.tsx 	modified:   app/favicon.ico 	modified:   app/globals.css 	modified:   app/layout.tsx 	deleted:    app/page.tsx 	new file:   components.json 	new file:   components/shared/MobileNav.tsx 	new file:   components/shared/Sidebar.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/sheet.tsx 	new file:   constants/index.ts 	new file:   lib/database/models/image.model.ts 	new file:   lib/database/models/transaction.model.ts 	new file:   lib/database/models/user.model.ts 	new file:   lib/database/mongoose.ts 	new file:   lib/utils.ts 	new file:   middleware.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   public/assets/icons/add.svg 	new file:   public/assets/icons/bag.svg 	new file:   public/assets/icons/camera.svg 	new file:   public/assets/icons/caret-down.svg 	new file:   public/assets/icons/check.svg 	new file:   public/assets/icons/close.svg 	new file:   public/assets/icons/coins.svg 	new file:   public/assets/icons/credit-coins.svg 	new file:   public/assets/icons/cross.svg 	new file:   public/assets/icons/download.svg 	new file:   public/assets/icons/filter.svg 	new file:   public/assets/icons/free-plan.svg 	new file:   public/assets/icons/home.svg 	new file:   public/assets/icons/image.svg 	new file:   public/assets/icons/menu.svg 	new file:   public/assets/icons/photo.svg 	new file:   public/assets/icons/profile.svg 	new file:   public/assets/icons/scan.svg 	new file:   public/assets/icons/search.svg 	new file:   public/assets/icons/spinner.svg 	new file:   public/assets/icons/stars.svg 	new file:   public/assets/images/banner-bg.png 	new file:   public/assets/images/gradient-bg.svg 	new file:   public/assets/images/logo-icon.svg 	new file:   public/assets/images/logo-text.svg 	new file:   public/assets/images/stacked-coins.png 	new file:   public/logo-icon.png 	new file:   public/logo.png 	modified:   tailwind.config.ts

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+/* 
+
+http://localhost:3000/transformations different =>
+http://localhost:3000/transformations/[id] Dynamic Routes. transformations folder add [id] folder and move here page.tsx file
+
+*/
